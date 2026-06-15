8196eac800db5bae5488544f22bcb94758b95aa7
joeycello1 <jos.a.harv@gmail.com>
2026-06-14 23:03:05 -0400

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from "path";
 
 export default defineConfig({
   base: '/kids-youtube-dashboard/',   // ⭐ ADD THIS LINE
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: path.resolve(__dirname, "src/lib")
+    }
+  }
 });
