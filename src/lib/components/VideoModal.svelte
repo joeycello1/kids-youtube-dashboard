ec0d12aadde42d0ba9139c71ddf5b9de7961ae65
joeycello1 <jos.a.harv@gmail.com>
2026-06-14 21:57:53 -0400

    Update VideoModal.svelte

--- src/lib/components/VideoModal.svelte
+++ src/lib/components/VideoModal.svelte
@@ -60,16 +60,10 @@
   import { WEBAPP_URL } from "$lib/config";
 
   async function handleBrokenVideo() {
-    // 1. Show kid-friendly message
+    // Show kid-friendly message
     errorMessage = "Oh drat! This video is Broken! Try another one.";
 
-    // 2. Mark locally
-    video.broken = true;
-
-    // 3. Update dashboard state
-    videos = [...videos];
-
-    // 4. Send to Apps Script
+    // Send to Apps Script
     await fetch(WEBAPP_URL, {
       method: "POST",
       body: JSON.stringify({
@@ -82,8 +76,8 @@
     });
   }
 
-  // 5. Close modal after a short delay
-  setTimeout(() => onClose(), 1200);
+  // Close modal after a short delay
+  setTimeout(() => onClose(), 9000);
 }
 
 
